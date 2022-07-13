797b5d9e18c4129de1df1c2e9dddbce0d7031cac
menuila <67451983+menuila@users.noreply.github.com>
2022-07-12 21:47:38 -0600

    Update and rename eks-cluster-deployment.tf to eks-2-cluster.tf

--- eks-2-cluster.tf
+++ eks-2-cluster.tf
@@ -17,7 +17,7 @@ module "eks" {
       instance_type                 = "t2.micro"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-deployment-2"
@@ -28,6 +28,38 @@ module "eks" {
     },
   ]
 }
+    
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = local.cluster_Development
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-Development-1"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 1
+    },
+    {
+      name                          = "worker-group-Development-2"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
