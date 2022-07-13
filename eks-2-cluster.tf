22bb5762632fcfb1a6e68aef0d2c61b4912d33e9
menuila <67451983+menuila@users.noreply.github.com>
2022-07-13 02:23:47 -0600

    Update eks-2-cluster.tf

--- eks-2-cluster.tf
+++ eks-2-cluster.tf
@@ -19,7 +19,7 @@ module "eks_deployment" {
       instance_type                 = "t2.micro"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-deployment-2"
