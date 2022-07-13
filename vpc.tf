701b4049230dff45e2b4792f4b49c423ca27ee01
menuila <67451983+menuila@users.noreply.github.com>
2022-07-12 21:57:00 -0600

    Update and rename vpc-deployment.tf to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,10 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "deployment-eks-${random_string.suffix.result}"
+  #cluster_deployment = "deployment-eks-${random_string.suffix.result}"
+  cluster_deployment = "deployment-cluster"
+  cluster_development = "development-cluster"
+
 }
 
 resource "random_string" "suffix" {
@@ -25,26 +28,27 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "deployment-vpc"
+  name                 = "devops-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  public_subnets       = ["10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24", "10.0.10.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${local.cluster_deployment}" = "shared"
+    #"kubernetes.io/cluster/${local.cluster_development}" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${local.cluster_deployment}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${local.cluster_deployment}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
